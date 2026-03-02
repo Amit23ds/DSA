ecc8ba26d2cc1db89f2f050836c5acebe070ec60
Amit Kumar Yadav <157636243+Amit23ds@users.noreply.github.com>
2026-03-03 02:03:37 +0530

    Time: 1 ms (65.38%) | Memory: 43.2 MB (83.71%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,5 +1,7 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        //if array is sorted means all the string value from 1st to last have common 
+        // means we have to check only 1st and last element of the array
        StringBuilder sb = new StringBuilder();
        Arrays.sort(strs);
        String first=strs[0];

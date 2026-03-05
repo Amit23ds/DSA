db0fa97ba86658a6c05a1475a2c3de08a1e08882
Amit Kumar Yadav <157636243+Amit23ds@users.noreply.github.com>
2026-03-05 16:19:34 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (99.88%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,8 +1,42 @@
 class Solution {
+    // public void removeDuplicates(int[] nums) {
+    //     int j=0;
+    //     for(int i=1;i<nums.length;i++){
+    //         if(nums[i]!=nums[j]){
+    //             j++;
+    //             nums[j]=nums[i];          
+    //         }
+    //     }
+    //     // return j+1;
+    // }
     public boolean search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                return true;
+        // removeDuplicates(nums);
+        int n=nums.length;
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid= low+(high-low)/2;
+            if(nums[mid]==target) return true;
+            if (nums[low] == nums[mid] && nums[mid]== nums[high]){
+                low++;
+                high--;
+                continue;
+            }
+            if(nums[low]<=nums[mid]){
+                if(target>=nums[low] && target<nums[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else{
+                if(target>=nums[mid] && target<=nums[high]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
             }
         }
         return false;

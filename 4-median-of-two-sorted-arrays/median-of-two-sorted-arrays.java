326b42e33d57a20bbc2f3266bde7e41052c1ba1e
Amit Kumar Yadav <157636243+Amit23ds@users.noreply.github.com>
2026-03-22 20:56:09 +0530

    Time: 1 ms (99.84%) | Memory: 49 MB (46.08%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,44 +1,34 @@
 class Solution {
-    private int[] merge(int[] nums1,int[] nums2){
-        int m=nums1.length;
-        int n=nums2.length;
-        int[] res= new int[m+n];
-        int i=0;
-        int j=0;
-        int k=0;
-        while(i<m && j<n){
-            if(nums1[i]<nums2[j]){
-                res[k]=nums1[i];
-                i++;
-            }else{
-                res[k]=nums2[j];
-                j++;
-            }
-            k++;
-        }
-        while(i<m){
-            res[k]=nums1[i];
-            i++;
-            k++;
-        }
-        while(j<n){
-            res[k]=nums2[j];
-            j++;
-            k++;
-        }
-        return res;
-    }
-
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        // int m=nums1.length;
-        // int n=nums2.length;
-        int[] res= merge(nums1,nums2);
-        // double low=0 , high = res[res.length-1];
-        // while()
-        int n=res.length;
-        if(n%2!=0){
-            return res[n/2];
+        int n=nums1.length, m=nums2.length;
+        if(n>m) return findMedianSortedArrays(nums2,nums1);
+        int lo=0;
+        int hi=n;
+        while(lo<=hi){
+            int mid1 = (lo + hi) / 2;
+            int mid2 = (n + m + 1) / 2 - mid1;
+
+            int l1 = (mid1 == 0) ? 
+                    Integer.MIN_VALUE : nums1[mid1 - 1];
+            int r1 = (mid1 == n) ? 
+                    Integer.MAX_VALUE : nums1[mid1];
+            
+            int l2 = (mid2 == 0) ? 
+                    Integer.MIN_VALUE : nums2[mid2 - 1];
+            int r2 = (mid2 == m) ? 
+                    Integer.MAX_VALUE : nums2[mid2];
+
+            if (l1 <= r2 && l2 <= r1) {
+                if ((n + m) % 2 == 0)
+                    return (Math.max(l1, l2)+Math.min(r1, r2))/2.0;
+                else
+                    return Math.max(l1, l2);
+            }
+            if (l1 > r2)
+                hi = mid1 - 1;
+            else
+                lo = mid1 + 1;
         }
-        return (res[(n - 1) / 2] + res[n / 2]) / 2.0;
+        return 0;
     }
 }

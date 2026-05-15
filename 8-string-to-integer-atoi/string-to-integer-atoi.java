99caff8c458c38ebe2888bb13f7325b5bdc8f360
Amit Kumar Yadav <157636243+Amit23ds@users.noreply.github.com>
2026-05-15 12:32:58 +0530

    Time: 1 ms (100.00%) | Memory: 43.5 MB (93.93%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,30 +1,28 @@
 class Solution {
     public int myAtoi(String s) {
-        int n=s.length();
-        if(n==0) return 0;
-        
-        int i=0;
-        while(s.charAt(i)==' ' && i<n-1){
+        int n = s.length();
+        int i = 0;
+        while (i < n && s.charAt(i) == ' ') {
             i++;
         }
-        int res=0;
-        int value=1;
-        if(s.charAt(i)=='-' || s.charAt(i)=='+'){
-            value=s.charAt(i)=='-' ? -1:1;
+        if (i == n) return 0;
+        int sign = 1;
+        if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+            sign = (s.charAt(i) == '-') ? -1 : 1;
             i++;
         }
-        while(i<n){
-            if(Character.isDigit(s.charAt(i))){
-               int x = s.charAt(i) - '0';
-                if (res > (Integer.MAX_VALUE - x) / 10){
-                    return value==-1?Integer.MIN_VALUE:Integer.MAX_VALUE;
-                }
-                res=res*10+x;
-                i++;
-            }else{
-                break;
-            }
+        return helper(s, i, sign, 0);
+    }
+
+    private int helper(String s, int i, int sign, int res) {
+        if (i >= s.length() || !Character.isDigit(s.charAt(i))) {
+            return res * sign;
         }
-        return (res*value);
+        int digit = s.charAt(i) - '0';
+        if (res > (Integer.MAX_VALUE - digit) / 10) {
+            return (sign == -1) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        }
+        res = res * 10 + digit;
+        return helper(s, i + 1, sign, res);
     }
 }

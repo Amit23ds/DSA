d8c1a5285bdc64f4d9086fd03a2ffeaea81f7739
Amit Kumar Yadav <157636243+Amit23ds@users.noreply.github.com>
2026-07-30 19:28:05 +0530

    Time: 3 ms (11.24%) | Memory: 43.1 MB (79.43%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -1,18 +1,38 @@
 class Solution {
     public int minimumPushes(String word) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
         int n =word.length();
-        int assignKey=2;
-        if(n<=8) return n;
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            if(assignKey >9){
-                assignKey=2;
-            }
-            hm.put(assignKey,hm.getOrDefault(assignKey,0)+1);
-            cnt+=hm.get(assignKey);
-            assignKey++; 
+        int[] freq= new int[26];
+        for(char ch : word.toCharArray()){
+            freq[ch-'a']++;
         }
-        return cnt;
+        Arrays.sort(freq);
+        int ans =0;
+        for(int i=25;i>=0;i--){
+            ans+=freq[i]*(((25-i)/8)+1);
+        }
+        return ans;
+
+
+        // if(n<=8) return n;
+        // else if(n>=9 && n<16) return 8+(n%8)*2;
+        // else if(n==16) return 24;
+        // else if(n>=17 && n<24) return 24+(n%8)*3;
+        // else if(n==24) return 48  ;
+        // return 48+(n%8)*4;
+        
+        
+        // HashMap<Integer, Integer> hm = new HashMap<>();
+        // int assignKey=2;
+        // if(n<=8) return n;
+        // int cnt=0;
+        // for(int i=0;i<n;i++){
+        //     if(assignKey >9){
+        //         assignKey=2;
+        //     }
+        //     hm.put(assignKey,hm.getOrDefault(assignKey,0)+1);
+        //     cnt+=hm.get(assignKey);
+        //     assignKey++; 
+        // }
+        // return cnt;
     }
 }

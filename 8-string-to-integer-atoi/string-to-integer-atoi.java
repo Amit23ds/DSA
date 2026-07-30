c222e4a550520ed0ec0268980bcc0c313390ad57
Amit Kumar Yadav <157636243+Amit23ds@users.noreply.github.com>
2026-07-30 22:27:15 +0530

    Time: 2 ms (11.24%) | Memory: 43.7 MB (76.27%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,28 +1,48 @@
 class Solution {
+    
     public int myAtoi(String s) {
-        int n = s.length();
-        int i = 0;
-        while (i < n && s.charAt(i) == ' ') {
+        s=s.trim();
+        int n=s.length();
+        if(s.length()==0) return 0;
+        int i=0;
+        int sign=1;
+        if(s.charAt(i)=='-'){
+            sign=-1;
+            i++;
+        }else if(s.charAt(i)=='+'){
+            sign=1;
+            i++;
+        }else if(!Character.isDigit(s.charAt(i))){
+            return 0;
+        }
+        long ans=0;
+        while(i<n){
+            char c=s.charAt(i);
+            if(Character.isDigit(c)){
+                int digit=c-'0';
+                if (ans > Integer.MAX_VALUE / 10 ||
+                    (ans == Integer.MAX_VALUE / 10 && digit > 7)){
+                        if(sign==1) return Integer.MAX_VALUE;
+                        else return Integer.MIN_VALUE;
+                }
+                ans=ans*10+digit;
+            }else if(!Character.isDigit(c)){
+                ans=ans*sign;
+                if(ans>Integer.MAX_VALUE){
+                    return Integer.MAX_VALUE;
+                }else if(ans<Integer.MIN_VALUE){
+                    return Integer.MIN_VALUE;
+                }
+                return (int)ans;
+            }
             i++;
         }
-        if (i == n) return 0;
-        int sign = 1;
-        if (s.charAt(i) == '+' || s.charAt(i) == '-') {
-            sign = (s.charAt(i) == '-') ? -1 : 1;
-            i++;
+        ans=ans*sign;
+        if(ans>Integer.MAX_VALUE){
+            return Integer.MAX_VALUE;
+        }else if(ans<Integer.MIN_VALUE){
+            return Integer.MIN_VALUE;
         }
-        return helper(s, i, sign, 0);
-    }
-
-    private int helper(String s, int i, int sign, int res) {
-        if (i >= s.length() || !Character.isDigit(s.charAt(i))) {
-            return res * sign;
-        }
-        int digit = s.charAt(i) - '0';
-        if (res > (Integer.MAX_VALUE - digit) / 10) {
-            return (sign == -1) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-        }
-        res = res * 10 + digit;
-        return helper(s, i + 1, sign, res);
+        return (int)ans;
     }
 }

47711955486f3cd26d96762aa71ea5e3ed1edb83
Amit Kumar Yadav <157636243+Amit23ds@users.noreply.github.com>
2026-08-04 20:48:41 +0530

    Time: 6 ms (39.24%) | Memory: 47.6 MB (5.33%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.java
+++ 4107-find-missing-elements/find-missing-elements.java
@@ -4,12 +4,6 @@ class Solution {
         Arrays.sort(nums);
         int n=nums.length;
         int min=nums[0], max=nums[n-1], i=1;
-        // for(int i=1;i<n;i++){
-        //     min++;
-        //     if(nums[i]!=min){
-        //         ans.add(min);
-        //     }
-        // }
         while(i<n){
             min++;
             if(nums[i]!=min){

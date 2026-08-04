4bc721b3568c3d8061684da1cd3ea69494352899
Amit Kumar Yadav <157636243+Amit23ds@users.noreply.github.com>
2026-08-05 00:57:07 +0530

    Time: 10 ms (6.20%) | Memory: 47.1 MB (5.07%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -3,32 +3,31 @@ class Solution {
         int sum = 0;
         for(int i=0;i<s.length();i++){
             char ch =s.charAt(i);
-            if(ch == '(')
+            if(ch== '(')
                 sum++;
             else
                 sum--;
-            if(sum < 0)
+            if(sum<0)
                 return false;
         }
-        return sum == 0;
+        return sum==0;
     }
     private void generate(String curr, int n, int length, List<String> result) {
-        if(length == 2 * n) {
+        if(length==2*n) {
             if(isValid(curr))
                 result.add(curr);
             return;
         }
+        curr+= '(';
+        generate(curr,n,length+1,result);
+        curr=curr.substring(0,curr.length()-1);
 
-        curr += '(';
-        generate(curr, n, length + 1, result);
-        curr = curr.substring(0, curr.length() - 1);
-
-        curr += ')';
-        generate(curr, n, length + 1, result);
+        curr+= ')';
+        generate(curr,n,length+1,result);
     }
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-        generate("", n, 0, result);
+        generate("",n,0,result);
         return result;
     }
 }

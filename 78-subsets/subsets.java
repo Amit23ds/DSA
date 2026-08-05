fd0b787bc982ebf1a0166bfaa016d6448cfa6304
Amit Kumar Yadav <157636243+Amit23ds@users.noreply.github.com>
2026-08-05 19:34:28 +0530

    Time: 1 ms (86.72%) | Memory: 44.2 MB (57.87%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,17 +1,33 @@
 class Solution {
+    // public List<List<Integer>> subsets(int[] nums) {
+    //     int n = nums.length;
+    //     int subsets = 1 << n; //2^n
+    //     List<List<Integer>> ans = new ArrayList<>();
+    //     for (int num = 0; num < subsets; num++) {
+    //         List<Integer> subset = new ArrayList<>();
+    //         for (int i = 0; i < n; i++) {
+    //             if ((num & (1 << i)) != 0) { // check the set bit
+    //                 subset.add(nums[i]);
+    //             }
+    //         }
+    //         ans.add(subset);
+    //     }
+    //     return ans;
+    // }
+    List<List<Integer>> result = new ArrayList<>();
+
     public List<List<Integer>> subsets(int[] nums) {
-        int n = nums.length;
-        int subsets = 1 << n;
-        List<List<Integer>> ans = new ArrayList<>();
-        for (int num = 0; num < subsets; num++) {
-            List<Integer> subset = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                if ((num & (1 << i)) != 0) { // check the set bit
-                    subset.add(nums[i]);
-                }
-            }
-            ans.add(subset);
+        helper(nums,0,new ArrayList<>());
+        return result;
+    }
+    void helper(int[] nums,int idx,List<Integer> temp){
+        if(idx>=nums.length){
+            result.add(new ArrayList<>(temp));
+            return;
         }
-        return ans;
+        helper(nums,idx+1,temp);
+        temp.add(nums[idx]);
+        helper(nums,idx+1,temp);
+        temp.remove(temp.size() - 1);
     }
 }

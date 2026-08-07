df94a750e74c573ac00688066673a9444d767c47
Amit Kumar Yadav <157636243+Amit23ds@users.noreply.github.com>
2026-08-07 14:58:45 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (83.08%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -0,0 +1,19 @@
+class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        int[] nums={1,2,3,4,5,6,7,8,9};
+        helper(nums,0,k,n,new ArrayList<>());
+        return result;
+    }
+    void helper(int[] nums,int idx,int k,int n, List<Integer> temp){
+        if(n==0 && temp.size()==k){
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+        if(idx>=nums.length || n<0) return;
+        helper(nums,idx+1,k,n,temp);
+        temp.add(nums[idx]);
+        helper(nums,idx+1,k,n-nums[idx],temp);
+        temp.remove(temp.size()-1);
+    }
+}

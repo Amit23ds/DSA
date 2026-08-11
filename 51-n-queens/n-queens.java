7969524042c0b50d2f07f33dc06ecc3f209b1b9c
Amit Kumar Yadav <157636243+Amit23ds@users.noreply.github.com>
2026-08-12 00:07:35 +0530

    Time: 1 ms (99.82%) | Memory: 46.5 MB (82.53%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,46 +1,39 @@
 class Solution {
-    List<List<String>> result = new ArrayList<>();
     public List<List<String>> solveNQueens(int n) {
-        if(n==0) return result;
-        List<String> board = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            StringBuilder row= new StringBuilder();
-            for(int j=0;j<n;j++){
-                row.append('.');
-            }
-            board.add(row.toString());
-        }
-        int startRow =0;
-        HashSet<Integer> cols = new HashSet<>();
-        HashSet<Integer> diags = new HashSet<>();
-        HashSet<Integer> antiDiags = new HashSet<>();
-        solve(board,startRow,cols,diags,antiDiags);
+        List<List<String>> res=new ArrayList<>();
+        char[][] board=new char[n][n];
+        for(char[] row:board) Arrays.fill(row,'.');
+        int[] leftRow=new int[n];
+        int[] lowerDiagonal=new int[2*n-1];
+        int[] upperDiagonal=new int[2*n-1];
 
-        return result;
+        solve(0,board,n,leftRow,lowerDiagonal,upperDiagonal,res);
+        return res;
     }
-    void solve(List<String> board,int row,HashSet<Integer> cols, HashSet<Integer> diags,HashSet<Integer> antiDiags){
-        if(row==board.size()){
-            result.add(new ArrayList<>(board));
+    void solve(int col,char[][] board,int n,int[] leftRow,int[] lowerDiagonal,
+                int[] upperDiagonal,List<List<String>> res
+                )
+    {
+        if(col==n){
+            List<String> temp=new ArrayList<>();
+            for(int i=0;i<n;i++){
+                temp.add(new String(board[i]));
+            }
+            res.add(temp);
             return;
         }
-        for(int col=0;col<board.size();col++){
-            int diagId=row-col;
-            int antiDiagId=row+col;
-            if(cols.contains(col) || diags.contains(diagId) || antiDiags.contains(antiDiagId)){
-                continue;
+        for(int row=0;row<n;row++){
+            if(leftRow[row]==0 && lowerDiagonal[row+col]==0 && upperDiagonal[n-1+col-row]==0){
+                board[row][col]='Q';
+                leftRow[row]=1;
+                lowerDiagonal[row+col]=1;
+                upperDiagonal[n-1+col-row]=1;
+                solve(col+1,board,n,leftRow,lowerDiagonal,upperDiagonal,res);
+                board[row][col]='.';
+                leftRow[row]=0;
+                lowerDiagonal[row+col]=0;
+                upperDiagonal[n-1+col-row]=0;
             }
-            cols.add(col);
-            diags.add(diagId);
-            antiDiags.add(antiDiagId);
-            StringBuilder newRow= new StringBuilder(board.get(row));
-            newRow.setCharAt(col,'Q');
-            board.set(row,newRow.toString());
-            solve(board,row+1,cols,diags,antiDiags);
-            cols.remove(col); 
-            diags.remove(diagId);
-            antiDiags.remove(antiDiagId);
-            newRow.setCharAt(col,'.');
-            board.set(row,newRow.toString());
         }
     }
 }

03482d3bfd5212ec31fee0aa5aa72d5e48de5e59
Amit Kumar Yadav <157636243+Amit23ds@users.noreply.github.com>
2026-08-11 22:47:02 +0530

    Time: 6 ms (14.29%) | Memory: 46.8 MB (28.91%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -10,7 +10,6 @@ class Solution {
             }
             board.add(row.toString());
         }
-        //solve(board,0);
         int startRow =0;
         HashSet<Integer> cols = new HashSet<>();
         HashSet<Integer> diags = new HashSet<>();
@@ -37,47 +36,11 @@ class Solution {
             newRow.setCharAt(col,'Q');
             board.set(row,newRow.toString());
             solve(board,row+1,cols,diags,antiDiags);
-            cols.remove(col);
+            cols.remove(col); 
             diags.remove(diagId);
             antiDiags.remove(antiDiagId);
             newRow.setCharAt(col,'.');
             board.set(row,newRow.toString());
         }
     }
-
-
-    // private void solve(List<String> board,int row){
-    //     if(row==board.size()){
-    //         result.add(new ArrayList<>(board));
-    //         return;
-    //     }
-    //     for(int i=0;i<board.size();i++){
-    //         if(isValid(board,row,i)){
-    //             StringBuilder newRow = new StringBuilder(board.get(row));
-    //             newRow.setCharAt(i,'Q');
-    //             board.set(row,newRow.toString());
-    //             solve(board,row+1);
-    //             newRow.setCharAt(i,'.');
-    //             board.set(row,newRow.toString());
-    //         }
-    //     }
-    // }
-    // boolean isValid(List<String> board,int row,int col){
-    //     for(int i=row;i>=0;i--){
-    //         if(board.get(i).charAt(col)=='Q'){
-    //             return false;
-    //         }
-    //     }
-    //     for(int i=row,j=col; i>=0 && j>=0; i--,j--){
-    //         if(board.get(i).charAt(j)=='Q'){
-    //             return false;
-    //         }
-    //     }
-    //     for(int i=row,j=col; i>=0 && j<board.size(); i--,j++){
-    //         if(board.get(i).charAt(j)=='Q'){
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-    // }
 }

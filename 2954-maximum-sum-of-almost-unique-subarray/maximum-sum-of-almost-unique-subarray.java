ad2167a866271e7ab166ef64a21a0cdcacc10a2f
Amit Kumar Yadav <157636243+Amit23ds@users.noreply.github.com>
2026-08-16 00:32:02 +0530

    Time: 27 ms (25.09%) | Memory: 48.8 MB (97.38%) - LeetSync

--- 2954-maximum-sum-of-almost-unique-subarray/maximum-sum-of-almost-unique-subarray.java
+++ 2954-maximum-sum-of-almost-unique-subarray/maximum-sum-of-almost-unique-subarray.java
@@ -8,10 +8,11 @@ class Solution {
             hm.put(nums.get(r),hm.getOrDefault(nums.get(r),0)+1);
             sum+=nums.get(r);
             while((r-l+1)>k){
-                sum-=nums.get(l);
-                hm.put(nums.get(l),hm.getOrDefault(nums.get(l),0)-1);
-                if(hm.get(nums.get(l))==0){
-                    hm.remove(nums.get(l));
+                int x=nums.get(l);
+                sum-=x;
+                hm.put(x,hm.getOrDefault(x,0)-1);
+                if(hm.get(x)==0){
+                    hm.remove(x);
                 }
                 l++;
             }

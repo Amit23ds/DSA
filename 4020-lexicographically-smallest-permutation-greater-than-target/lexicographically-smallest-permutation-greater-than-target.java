1eccadcaf532a66845fb44b7dc40637525a2be33
Amit Kumar Yadav <157636243+Amit23ds@users.noreply.github.com>
2026-08-27 20:21:57 +0530

    Time: 3 ms (58.06%) | Memory: 44.5 MB (90.32%) - LeetSync

--- 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.java
+++ 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.java
@@ -2,14 +2,14 @@ class Solution {
     String res="";
 
     boolean solve(StringBuilder curr,int[] count,String target,int i,boolean greater) {
-        if(i==target.length()) {
-            if(greater) {
+        if(i==target.length()){
+            if(greater){
                 res=curr.toString();
                 return true;
             }
             return false;
         }
-        for(char ch='a';ch<='z';ch++) {
+        for(char ch='a';ch<='z';ch++){
             if(count[ch-'a']==0) continue;
             if(greater==false && ch<target.charAt(i)) continue;
 
@@ -18,7 +18,7 @@ class Solution {
 
             boolean isGreater=greater || ch>target.charAt(i);
 
-            if(solve(curr,count,target,i+1,isGreater)) {
+            if(solve(curr,count,target,i+1,isGreater)){
                 return true;
             }
             curr.deleteCharAt(curr.length()-1);
@@ -29,7 +29,7 @@ class Solution {
 
     public String lexGreaterPermutation(String s, String target) {
         int[] freq =new int[26];
-        for (char ch:s.toCharArray()){
+        for(char ch:s.toCharArray()){
             freq[ch-'a']++;
         }   
         StringBuilder curr = new StringBuilder();

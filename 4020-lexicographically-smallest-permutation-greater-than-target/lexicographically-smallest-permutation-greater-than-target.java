e254a779171ed63def9925d617dbd05e5f667d6d
Amit Kumar Yadav <157636243+Amit23ds@users.noreply.github.com>
2026-08-27 20:20:02 +0530

    Time: 3 ms (58.06%) | Memory: 44.7 MB (83.87%) - LeetSync

---
+++ 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.java
@@ -0,0 +1,39 @@
+class Solution {
+    String res="";
+
+    boolean solve(StringBuilder curr,int[] count,String target,int i,boolean greater) {
+        if(i==target.length()) {
+            if(greater) {
+                res=curr.toString();
+                return true;
+            }
+            return false;
+        }
+        for(char ch='a';ch<='z';ch++) {
+            if(count[ch-'a']==0) continue;
+            if(greater==false && ch<target.charAt(i)) continue;
+
+            curr.append(ch);
+            count[ch-'a']--;
+
+            boolean isGreater=greater || ch>target.charAt(i);
+
+            if(solve(curr,count,target,i+1,isGreater)) {
+                return true;
+            }
+            curr.deleteCharAt(curr.length()-1);
+            count[ch-'a']++;
+        }
+        return false;
+    }
+
+    public String lexGreaterPermutation(String s, String target) {
+        int[] freq =new int[26];
+        for (char ch:s.toCharArray()){
+            freq[ch-'a']++;
+        }   
+        StringBuilder curr = new StringBuilder();
+        solve(curr,freq,target,0,false);
+        return res;
+    }
+}

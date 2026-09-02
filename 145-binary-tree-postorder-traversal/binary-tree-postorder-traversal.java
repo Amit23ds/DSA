7f4ad6c883fae745d8a64ce99345297a0c12763f
Amit Kumar Yadav <157636243+Amit23ds@users.noreply.github.com>
2026-09-02 13:51:20 +0530

    Time: 1 ms (11.98%) | Memory: 43.1 MB (52.54%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -14,18 +14,57 @@
  * }
  */
 class Solution {
-    List<Integer> res=new ArrayList<>();
+    //List<Integer> res=new ArrayList<>();
     public List<Integer> postorderTraversal(TreeNode root) {
-        postorder(root);
-        return res;
-    }
-    void postorder(TreeNode root){
-        if(root==null){
-            return;
+        //postorder(root);
+        //return res;
+        // List<Integer> postorder=new ArrayList<>();
+        // Stack<TreeNode> st1=new Stack<>();
+        // Stack<TreeNode> st2=new Stack<>();
+
+        // if(root==null) return postorder;
+        // st1.push(root); 
+        // while(!st1.isEmpty()){
+        //     root=st1.pop();
+        //     st2.add(root);
+        //     if(root.left!=null) st1.push(root.left);
+        //     if(root.right!=null) st1.push(root.right);
+        // }
+        // while(!st2.isEmpty()){
+        //     postorder.add(st2.pop().val);
+        // }
+        // return postorder;
+        List<Integer> postorder=new ArrayList<>();
+        Stack<TreeNode> st=new Stack<>();
+        TreeNode curr=root;
+        while(curr!=null || !st.isEmpty()){
+            if(curr!=null){
+                st.push(curr);
+                curr=curr.left;
+            }else{
+                TreeNode temp=st.peek().right;
+                if(temp==null){
+                    temp=st.pop();
+                    postorder.add(temp.val);
+                    while(!st.empty() && temp==st.peek().right){
+                        temp=st.pop();
+                        postorder.add(temp.val);
+                    }
+                }else{
+                    curr=temp;
+                }
+            }
         }
-        postorder(root.left);
-        postorder(root.right);
-        res.add(root.val);
+        return postorder;
 
     }
+    // void postorder(TreeNode root){
+    //     if(root==null){
+    //         return;
+    //     }
+    //     postorder(root.left);
+    //     postorder(root.right);
+    //     res.add(root.val);
+
+    // }
 }
